26f7c39cc0d09e814ed7dd3ccd108d83bf276e91
riqsans <thrq88@gmail.com>
2025-11-24 09:14:53 +0700

    menambahkan file komik.js dan db.js

---
+++ models/komik.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define('Komik', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        author: {
+            type: DataTypes.STRING,
+        },
+        imageType: DataTypes.STRING,
+        imageName: DataTypes.STRING,
+        ImageData: DataTypes.BLOB('long'),
+        
+    }, 
+    {
+        tableName: 'komik',
+    });
+
+    return Komik;
+}
